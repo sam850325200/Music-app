c18bf764eed8596b9d1d2aef0f258c195fd81298
司宇軒 <sam850325200@gmail.com>
2020-04-15 10:54:45 +0800

    Initial Commit

---
+++ Music app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Music app
+//
+//  Created by 司宇軒 on 2020/4/15.
+//  Copyright © 2020 fubonlife. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
